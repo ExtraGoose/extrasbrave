e18acf84e2857413d95f92186c0aca0cb3fa7eb1
ExtraGoose <106400128+ExtraGoose@users.noreply.github.com>
2023-10-31 00:19:07 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /* Когда пользователь нажимает на кнопку,
 переключение между скрытием и отображением раскрывающегося содержимого */
-function myFunction() {
+function extramenu() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
